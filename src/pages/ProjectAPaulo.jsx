0af515e431e952c2c63777b05d7141e4131e1133
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-08-06 15:12:10 +0200

    err fixed

--- src/pages/ProjectAPaulo.jsx
+++ src/pages/ProjectAPaulo.jsx
@@ -190,7 +190,7 @@ export default function ProjectAPaulo() {
         <Grid item xs={12} md={6}>
           <Card sx={{ borderRadius: "1rem", overflow: "hidden", boxShadow: 3 }}>
             <img
-              src="photos/0TS_5060.jpg"
+              src="photos/4355lecie.jpg"
               style={{ width: "100%", height: "auto", display: "block" }}
             />
           </Card>
